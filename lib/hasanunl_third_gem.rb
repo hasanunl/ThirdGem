35e6700089f57368ca74d48aa841f63c3810e49e
hasanunl <hu1997tr@gmail.com>
2018-10-10 11:39:57 +0300

    Completed third gem

---
+++ lib/hasanunl_third_gem.rb
@@ -0,0 +1,12 @@
+require "hasanunl_third_gem/version"
+
+module HasanunlThirdGem
+  class WhoIs
+    def self.awesome?
+      puts "YOU ARE AWESOME!!"
+    end
+    def self.who(me)
+      puts me
+    end
+  end
+end
---
+++ lib/hasanunl_third_gem/version.rb
@@ -0,0 +1,3 @@
+module HasanunlThirdGem
+  VERSION = "0.1.0"
+end
